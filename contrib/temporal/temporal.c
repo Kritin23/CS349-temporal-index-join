238028eec1380cd9f67950ab23fac73ef668ed44
Kritin Gupta <kritin.gupta.23@gmail.com>
2026-04-09 00:38:25 +0530

    impl compress

--- contrib/temporal/temporal.c
+++ contrib/temporal/temporal.c
@@ -13,9 +13,14 @@ typedef struct temporalKey {
     Timestamp time_upper;
 } temporalKey;
 
+typedef struct leafKey {
+    int64 id;
+    Timestamp start;
+    Timestamp end;
+} leafKey;
 
 
-PG_FUNCTION_INFO_V1(temporal_compress);
+PG_FUNCTION_INFO_V1(my_compress);
 
 Datum
 temporal_compress(PG_FUNCTION_ARGS)
@@ -27,13 +32,14 @@ temporal_compress(PG_FUNCTION_ARGS)
     {
         /* replace entry->key with a compressed version */
         temporalKey *key = palloc(sizeof(temporalKey));
-
-        // ****** TODO ******
-        // Figure out what do we get in entry, and how to fill 
-        // it in temporalKey.
+        leafKey *entry_data = (leafKey *)(entry->key);
+        key->id_lower   = entry_data->id;
+        key->id_upper   = entry_data->id;
+        key->time_lower = entry_data->start;
+        key->time_upper = entry_data->end;
 
         /* fill *compressed_data from entry->key ... */
-        
+
         retval = palloc(sizeof(GISTENTRY));
         gistentryinit(*retval, PointerGetDatum(key),
                       entry->rel, entry->page, entry->offset, false);
