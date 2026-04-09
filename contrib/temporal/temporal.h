fd67f9eaf85f46518ae74714988deec425a80752
Kritin Gupta <kritin.gupta.23@gmail.com>
2026-04-09 20:22:27 +0530

    sql code to call c code

--- contrib/temporal/temporal.h
+++ contrib/temporal/temporal.h
@@ -6,5 +6,18 @@
 #ifndef __TEMPORAL__
 #define __TEMPORAL__
 
+#define TempRangeOverlap        1
+#define TempPointOverlap        2
+#define TempIdxPointOverlap     3
+#define TempIdxRangeOverlap     4
+#define TempRangeContains       5
+#define TempPointContains       6
+#define TempIdxPointContains    7
+#define TempIdxRangeContains    8
+#define TempRangeContained      9
+#define TempPointContained      10
+#define TempIdxPointContained   11
+#define TempIdxRangeContained   12
+
 
 #endif __TEMPORAL
